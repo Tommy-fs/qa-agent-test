6b994b2bbf8f43f7de20f7593130ef3b99c28cab
Tommy-fs <837893285@qq.com>
2025-02-19 09:55:50 +0800

    agent core

---
+++ agent_tool/example1.py
@@ -0,0 +1,13 @@
+# example1.py
+
+from agent_core.agents import Agent
+
+
+def main():
+    agent = Agent(model_name="gpt-4o-mini")
+    agent.execute("Who are you?")
+    print(f"Response: {agent.execution_responses}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ example1.py
@@ -0,0 +1,13 @@
+# example1.py
+
+from agent_core.agents import Agent
+
+
+def main():
+    agent = Agent(model_name="gemini-1.5-flash-002")
+    agent.execute("Who are you?")
+    print(f"Response: {agent.execution_responses}")
+
+
+if __name__ == "__main__":
+    main()
